7343f18da4fab3b4a75fc653a7af12b898f9b9c8
MacSaintcode <damimakanju@gmail.com>
2023-12-11 11:31:47 -1100

    Implementing countdown

--- app/src/main/java/com/example/triv/MainActivity.java
+++ app/src/main/java/com/example/triv/MainActivity.java
@@ -4,16 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import  java.lang.Object;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private
 
+    TextView counttext;
+    private CountDownTimer time;
+    private long Milliseconds=20000;
+    boolean running=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        countDownText;
+        counttext=findViewById(R.id.timer);
+
+
+
+    }
+    void starttimer(){
+        time=new CountDownTimer(Milliseconds,1000) {
+            @Override
+            public void onTick(long l) {
+                Milliseconds=l;
+                updatetimer();
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+        running=true;
+    }
+
+    public void updatetimer(){
+        int second=(int) Milliseconds % 20000 / 1000;
+        String tim ="";
+        if(second<10){
+            tim+="0";
+            tim+=second;
+        }
+        counttext.setText(tim);
 
     }
 
