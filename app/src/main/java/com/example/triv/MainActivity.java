c8c142cc433326fb442a1d22d881e674876402f2
MacSaintcode <damimakanju@gmail.com>
2023-12-11 09:36:21 -1100

    adding timer

--- app/src/main/java/com/example/triv/MainActivity.java
+++ app/src/main/java/com/example/triv/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.triv;
 
 import androidx.appcompat.app.AppCompatActivity;
+import  java.lang.Object;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        countDownText;
+
     }
+
 }
