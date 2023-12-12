fccb18d2eca16af3c02dfdb4e6a901bb973d81e1
MacSaintcode <damimakanju@gmail.com>
2023-12-11 17:10:28 -1100

    adding back button explicit intent

--- app/src/main/java/com/example/triv/register_login.java
+++ app/src/main/java/com/example/triv/register_login.java
@@ -2,13 +2,31 @@ package com.example.triv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class register_login extends AppCompatActivity {
+
+    ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_login);
+
+        back=findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent call=new Intent(register_login.this,MainMenu.class);
+            }
+        });
+
     }
+
+
+
 }
