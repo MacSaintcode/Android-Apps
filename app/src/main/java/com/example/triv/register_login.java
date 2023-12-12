213b0aba0a9a7738a9536f0eff824e99d53db21a
MacSaintcode <damimakanju@gmail.com>
2023-12-11 17:22:36 -1100

    adding forgot and create password text explicit intent

--- app/src/main/java/com/example/triv/register_login.java
+++ app/src/main/java/com/example/triv/register_login.java
@@ -2,26 +2,45 @@ package com.example.triv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class register_login extends AppCompatActivity {
 
     ImageView back;
+    TextView forgot;
+    TextView account;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_login);
 
         back=findViewById(R.id.back);
+        forgot=  findViewById(R.id.fpassword);
+        account= findViewById(R.id.caccount);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent call=new Intent(register_login.this,MainMenu.class);
+            }
+        });
+        forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent call=new Intent(register_login.this,ForgotPassword.class);
+            }
+        });
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent call=new Intent(register_login.this,New_user.class);
             }
         });
 
